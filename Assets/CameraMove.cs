8294354cd709dc022302f2c6a33451b84570cc44
Daniel Wu <binwu@ea.com>
2020-05-14 16:32:49 +0800

    updating

---
+++ Assets/CameraMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private Vector3 originalCam;
+    private Vector3 originalMouse;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 delNor = Camera.main.ScreenToViewportPoint(Input.mousePosition) - originalMouse;
+            delNor.z = 0;
+            Vector3 newPos = transform.position + delNor;
+            transform.position = Vector3.Lerp(originalCam, newPos, 0.5f);
+        }
+        else
+        {
+            originalCam = transform.position;
+            originalMouse = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        }
+    }
+}
---
+++ Assets/LightControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private Vector3 originalLightEuler;
+    private Vector3 originalMouse;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            transform.Rotate(new Vector3(Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0) * Time.deltaTime * 20f);
+        }
+    }
+}
